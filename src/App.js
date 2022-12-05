92685e4f3c7fa337e2883e983dd029efaf781b74
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-12-05 11:37:35 -0600

    hero added - dc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import { hydrateRoot } from "react-dom/client";
+import Hero from "./components/Hero";
+import "./global.css";
 
 function App() {
-  return <h1> Josh is a little bitch</h1>;
+  return (
+    <>
+      <Hero src="hero1.mp4" />
+      {/* <h1> Josh is a little bitch</h1> */}
+    </>
+  );
 }
 
 export default App;
