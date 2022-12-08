160ae84ba5856281155c1f6ac443afa370506aa9
Joshua Tully <jtully97@gmail.com>
2022-12-07 19:49:16 -0600

    navigation 1.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { hydrateRoot } from "react-dom/client";
+import  Navigation from './components/Navigation.jsx'
 
 function App() {
-  return <h1> Josh is a little bitch</h1>;
+  return (
+    <>
+    <Navigation />
+    <h1> Josh is a little bitch</h1>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/navigation.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    background-color: grey;
+    height: 72px;
+    padding: 0 25px;
+
+`
+
+function Navigation() {
+  return (
+    <Nav>
+        <ul>
+            
+        </ul>
+    </Nav>
+  )
+}
+
+export default Navigation
