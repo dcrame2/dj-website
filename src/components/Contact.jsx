45386d8b4045f478ed86481e46500f1594df608e
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-12-10 22:40:22 -0600

    contact form new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Variables } from "../styles/Variables";
 import { MediaQueries } from "../styles/Utilities";
+import LinkButton from "../sub_components/LinkButton";
 
 const Section = styled.section`
   height: auto;
@@ -13,20 +14,35 @@ const Section = styled.section`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 10px;
+    gap: 20px;
 
     div {
       display: flex;
-      gap: 20px;
+      flex-direction: column;
+      gap: 10px;
 
       label {
+        width: 150px;
         font-size: 2rem;
+        text-align: left;
       }
 
       input,
       textarea {
+        text-indent: 10px;
+        outline: none;
+        font-size: 1.5rem;
         background-color: transparent;
-        border-color: ${Variables.color2};
+        border-radius: 8px;
+        width: 400px;
+        border: 2px solid ${Variables.color2};
+      }
+      input {
+        height: 40px;
+      }
+
+      textarea {
+        height: 75px;
       }
     }
   }
@@ -36,23 +52,30 @@ const Contact = () => {
   return (
     <Section>
       <form action="https://formsubmit.co/dcrame2@gmail.com" method="POST">
+        <input type="hidden" name="_subject" value="New DJ Submission!" />
+        <input type="hidden" name="_cc" value="jtully@gmail.com" />
+        <input type="hidden" name="_captcha" value="false" />
+        {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"></input> */}
+
         <div>
-          <label htmlFor="fname">First Name:</label>
+          <label htmlFor="fname">First Name:*</label>
           <input type="text" name="fname" required />
         </div>
         <div>
-          <label htmlFor="lname">Label Name:</label>
+          <label htmlFor="lname">Label Name:*</label>
           <input type="text" name="lname" required />
         </div>
         <div>
-          <label htmlFor="email">Email Address:</label>
+          <label htmlFor="email">Email Address:*</label>
           <input type="email" name="email" required />
         </div>
         <div>
-          <label htmlFor="message">Message:</label>
+          <label htmlFor="message">Message:*</label>
           <textarea type="text" name="message" required />
         </div>
-        <button type="submit">Send</button>
+        <LinkButton text="Submit">
+          <button type="submit"></button>
+        </LinkButton>
       </form>
     </Section>
   );
