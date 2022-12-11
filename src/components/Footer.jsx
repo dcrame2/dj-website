0776a151f87f9674d461ac2c6c69bcb0d89af40f
Joshua Tully <jtully97@gmail.com>
2022-12-11 10:54:39 -0600

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
+import styled from 'styled-components';
+import { Variables } from '../styles/Variables';
+import { MediaQueries } from '../styles/Utilities';
+
 export default function Footer() {
     return <div>Footer</div>;
 }
