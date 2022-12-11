a1d79febe1c923ccb4ea8b9f4d147dd176b0edc3
Joshua Tully <jtully97@gmail.com>
2022-12-11 12:27:17 -0600

    update to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { MediaQueries, Container } from '../styles/Utilities';
 const FooterElm = styled.footer`
     position: relative;
     height: 60vh;
+    background-color: ${Variables.black};
 `;
 
 const FooterInner = styled.div`
@@ -13,6 +14,14 @@ const FooterInner = styled.div`
     .bottom-color-strip {
         width: 100%;
         position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 1;
+        background-color: ${Variables.color2};
+        padding: 20px 0;
+    }
+    .color-panel {
     }
 `;
 
@@ -20,7 +29,8 @@ export default function Footer() {
     return (
         <FooterElm>
             <FooterInner>
-                <div className='bottom-color-strip'></div>
+                <div className='bottom-color-strip' />
+                <div className='color-panel' />
             </FooterInner>
         </FooterElm>
     );
