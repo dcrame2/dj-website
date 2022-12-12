e4d9b5ce4d2baeab999afdddfa220acf46406afe
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-12-11 22:05:55 -0600

    new logo

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -4,35 +4,36 @@ export const tabletWidthInt = 1099;
 export const mobileWidthInt = 499;
 
 export const Variables = {
-    maxWidth: `${maxWidthInt}px`,
-    desktopW: `${desktopWidthInt}px`,
-    tabletWidth: `${tabletWidthInt}px`,
-    mobileWidth: `${mobileWidthInt}px`,
+  maxWidth: `${maxWidthInt}px`,
+  desktopW: `${desktopWidthInt}px`,
+  tabletWidth: `${tabletWidthInt}px`,
+  mobileWidth: `${mobileWidthInt}px`,
 
-    // colors
-    // When swapping leave note of what the color looks like
-    // used in button link styling
-    color1: '#E5B8F4', // light purple
-    color2: '#C147E9', // purple
-    color3: '#FCF9BE', // light yellow-white
-    color4: '#46C2CB', // teal aqua
-    color5: '#4D4C7D', // pale purple
-    color6: '#E9D5DA', // light pale pink
-    color7: '#FFFBE7', // white with hint of pink
-    color8: '#DDDDDD', // slate white
-    color9: '#FF6D28', // orange
-    color10: '#472183', //dark purple
-    //
-    black: 'black',
-    white: '#ffff',
+  // colors
+  // When swapping leave note of what the color looks like
+  // used in button link styling
+  color1: "#E5B8F4", // light purple
+  color2: "#C147E9", // purple
+  color3: "#FCF9BE", // light yellow-white
+  color4: "#46C2CB", // teal aqua
+  color5: "#4D4C7D", // pale purple
+  color6: "#E9D5DA", // light pale pink
+  color7: "#FFFBE7", // white with hint of pink
+  color8: "#DDDDDD", // slate white
+  color9: "#FF6D28", // orange
+  color10: "#472183", //dark purple
+  //
+  black: "black",
+  white: "#ffff",
 
-    // Type
-    joseph: 'Josefin Sans, sans-serif',
+  // Type
+  joseph: "Josefin Sans, sans-serif",
 
-    // Company name
-    companyName: 'Digital Jam',
+  // Company name
+  companyName: "Digital Jam",
 
-    // Logos
-    logo1Url: 'digitaljam-logo.png',
-    logo2Url: 'digitaljam-logo-removebg-preview.png',
+  // Logos
+  logo1Url: "digitaljam-logo.png",
+  logo2Url: "digitaljam-logo-removebg-preview.png",
+  logo3Url: "white-long.png",
 };
