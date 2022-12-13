882a63a910888c6b474348ebb95ca392228211fd
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-12-12 19:38:15 -0600

    updqte

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -32,7 +32,7 @@ export const Variables = {
   joseph: "Josefin Sans, sans-serif",
 
   // Company name
-  companyName: "Digital Jam",
+  companyName: "Digital Jammmm",
 
   // Logos
   logo1Url: "digitaljam-logo.png",
