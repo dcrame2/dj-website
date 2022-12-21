f231765bae53710f55b015f583b78005b0edc959
Joshua Tully <jtully97@gmail.com>
2022-12-20 20:35:12 -0600

    update

--- src/sub_components/Dropdown.jsx
+++ src/sub_components/Dropdown.jsx
@@ -7,24 +7,40 @@ const Container = styled.div`
     display: relative;
     border-bottom: 2px solid ${Variables.black};
 
-    button {
-        background-color: unset;
-        border: unset;
-        ${H3Styles}
-        color: ${Variables.black};
+    .wrapper {
+        .toggle {
+            background-color: unset;
+            border: unset;
+            ${H3Styles}
+            color: ${Variables.black};
+            padding: 20px 0;
+            max-width: 90%;
+            text-align: left;
+        }
+        .indicator {
+        }
     }
 
-    div.panel {
+    .panel {
         max-height: 0px;
         transition: max-height ease 0.4s;
         overflow: hidden;
 
         p {
             ${PBaseStyles}
+            margin-bottom: 24px;
+            opacity: 0;
+            transition: opacity ease 0.4s;
         }
         &.active {
             max-height: 200px;
             transition: max-height ease 0.4s;
+
+            p {
+                opacity: 1;
+
+                transition: opacity ease 0.4s;
+            }
         }
     }
 `;
@@ -39,8 +55,13 @@ export default function Dropdown({ ...props }) {
     };
 
     return (
-        <Container>
-            <button onClick={toggleDropdown}>{props.question}</button>
+        <Container className={props.className}>
+            <div className='wrapper'>
+                <button className='toggle' onClick={toggleDropdown}>
+                    {props.question}
+                </button>
+                <div className='indicator'></div>
+            </div>
             <div className={`panel ${active ? 'active' : ''}`}>
                 <p>{props.answer}</p>
             </div>
