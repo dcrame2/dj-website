aeb557b3bb750e69cea3f97527e2843aa499cb28
Joshua Tully <jtully97@gmail.com>
2023-01-31 21:56:13 -0600

    service module updates and refactor, pull out card and create a component of its own

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react';
 import { MediaQueries } from '../styles/Utilities';
-import { Variables } from '../styles/Variables';
+import { Variables, tabletWidthInt } from '../styles/Variables';
 import { Container } from '../styles/Utilities';
 import styled from 'styled-components';
 import { H3Styles, H2Styles, PSecondary } from '../styles/Type';
 import { useState, useEffect } from 'react';
 import LinkButton from '../sub_components/LinkButton';
-import { motion, useInView } from 'framer-motion';
-import Zoom from 'react-reveal/Zoom';
+import { motion, useInView, useAnimationControls } from 'framer-motion';
+import Card from '../sub_components/Card';
 
 // Google analytics
 const Section = styled.section`
@@ -62,6 +62,7 @@ const Section = styled.section`
             justify-content: space-between;
             gap: 42px;
             justify-content: center;
+            align-items: start;
 
             @media ${MediaQueries.tablet} {
                 gap: 42px;
@@ -71,120 +72,6 @@ const Section = styled.section`
             @media ${MediaQueries.mobile} {
                 padding-right: unset;
                 padding-left: unset;
-            }
-
-            button {
-                border: unset;
-                display: flex;
-                flex-wrap: wrap;
-                height: auto;
-                background-color: ${Variables.color12};
-                border: 1px solid ${Variables.color2};
-                color: ${Variables.white};
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                text-align: center;
-                gap: 10px;
-                padding: 45px;
-                border-radius: 40px;
-                max-width: 540px;
-                transition: background-color ease 0.7s;
-
-                box-shadow: 1px 1px 10px 2px ${Variables.color18};
-                -webkit-box-shadow: 1px 1px 10px 2px ${Variables.color18};
-                -moz-box-shadow: 1px 1px 10px 2px ${Variables.color18};
-
-                box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-                    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
-                    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-
-                @media ${MediaQueries.desktop} {
-                    flex: 1 1 0px;
-                }
-
-                @media ${MediaQueries.mobile} {
-                    border-radius: 30px;
-                }
-
-                &:hover
-            /*&:focus*/ {
-                    @media ${MediaQueries.desktop} {
-                        background-color: ${Variables.color10};
-                        transition: background-color ease 0.45s;
-                    }
-
-                    img {
-                        transform: rotateY(360deg);
-                        /* top: -75px; */
-                        transition: 3s;
-                    }
-                }
-                @media (max-width: 767px) {
-                    width: unset;
-                    padding: 34px;
-                }
-                img {
-                    position: relative;
-                    max-width: 120px;
-                    height: auto;
-                    opacity: 0.8;
-
-                    @media ${MediaQueries.tablet} {
-                        max-width: 150px;
-                    }
-                    @media ${MediaQueries.mobile} {
-                        max-width: 100px;
-                    }
-                }
-                &:nth-child(1) {
-                    span.num {
-                        ${H2Styles}
-                        color: ${Variables.color4};
-                    }
-                }
-                &:nth-child(2) {
-                    span.num {
-                        ${H2Styles}
-                        color: ${Variables.color3};
-                    }
-                }
-                &:nth-child(3) {
-                    span.num {
-                        ${H2Styles}
-                        color: ${Variables.color1};
-                    }
-                }
-                h2 {
-                    ${H3Styles}
-                    color: ${Variables.white};
-                    text-transform: uppercase;
-                }
-
-                .dropdown {
-                    display: flex;
-                    flex-direction: column;
-                    gap: 25px;
-                    max-height: 0;
-                    opacity: 0;
-                    transition: max-height 0.26s ease, opacity ease 0.25s;
-                    p {
-                        /* color: ${Variables.color1}; */
-                        font-family: ${Variables.joseph};
-                        font-size: 1.75rem;
-                        line-height: 25px;
-                        overflow: hidden;
-                    }
-                }
-                &.active {
-                    transition: background-color ease 0.45s;
-                    background-color: ${Variables.color16};
-                    .dropdown {
-                        max-height: 245px; // TODO: find solution to using auto height :)
-                        transition: max-height 0.25s ease, opacity ease 0.26s;
-                        opacity: 1;
-                    }
-                }
             }
         }
     }
@@ -218,9 +105,15 @@ const data = {
     subheading: `Managing a business demands a lot. Save resources and work with ${Variables.companyName} to help build and expand your businesses online presence. Our focal services are designing, developing and providing support but we can provide managed services for any web or mobile related projects.`, //We offer a wide range of web related services that are a must-have for any business.
 };
 
-const Services = () => {
+export default function Services() {
     const [active, setActive] = useState(0);
-    const [loaded, setLoaded] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+    const [windowSize, setWindowSize] = useState(0);
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, amount: 1 });
+    const [isInViewFired, setIsInViewFired] = useState(false);
+    const [initial, setInitial] = useState({ opacity: 0, scale: 0.5 });
+    const controls = useAnimationControls();
 
     const activeToggle = (index) => {
         if (index === active) {
@@ -229,25 +122,46 @@ const Services = () => {
             setActive(index);
         }
     };
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
 
     useEffect(() => {
-        setLoaded(true);
-    }, [loaded]);
+        setWindowSize(window.innerWidth);
+
+        function handleResize() {
+            setWindowSize(window.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        if (windowSize < tabletWidthInt) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [windowSize]);
+
+    useEffect(() => {
+        if (isInView) {
+            setIsInViewFired(true);
+            controls.start({
+                opacity: 1,
+                scale: 1,
+            });
+        }
+
+        if (isInViewFired) {
+            setInitial({ opacity: 1, scale: 1 });
+        }
+    }, [isInView, isInViewFired]);
 
     return (
         <Section id='services' className={active ? 'active' : ''}>
             <div className='inner-section'>
-                <div ref={ref} className='heading-container'>
-                    <motion.h2
-                        style={{
-                            transform: isInView ? 'none' : 'translateY(-200px)',
-                            opacity: isInView ? 1 : 0,
-                            transition:
-                                'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
-                        }}
-                    >
+                <div className='heading-container'>
+                    <motion.h2 initial={initial} animate={controls} ref={ref}>
                         {data.heading}
                     </motion.h2>
                     <h3>{data.subheading}</h3>
@@ -255,34 +169,16 @@ const Services = () => {
                 <div className='ServiceContainer'>
                     {data.services.map((service, i) => {
                         return (
-                            <Zoom key={`service-zoom-${i}`}>
-                                <button
-                                    className={active === i ? 'active' : ''}
-                                    onClick={() => activeToggle(i)}
-                                    key={`button-${i}`}
-                                >
-                                    <img
-                                        src={`${service.img}`}
-                                        alt={service.alt}
-                                    />
-                                    <span className='num'>{service.num}</span>
-                                    <h2>{service.name}</h2>
-                                    <span className='dropdown'>
-                                        <p>{service.desc}</p>
-                                        <LinkButton
-                                            text='Learn More'
-                                            ariaLabel={`Learn more about ${service.name}`}
-                                            href={`#${service.name.toLowerCase()}Section`}
-                                        ></LinkButton>
-                                    </span>
-                                </button>
-                            </Zoom>
+                            <Card
+                                data={service}
+                                active={active}
+                                onClick={activeToggle}
+                                index={i}
+                            />
                         );
                     })}
                 </div>
             </div>
         </Section>
     );
-};
-
-export default Services;
+}
