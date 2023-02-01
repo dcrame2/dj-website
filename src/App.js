ae3a7aabc590ce1bb39874185462820d6683dc05
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-31 20:00:59 -0600

    add react router

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ import { useState } from "react";
 import { MediaQueries } from "./styles/Utilities";
 import { Variables } from "./styles/Variables.jsx";
 import styled from "styled-components";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  HashRouter,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AboutUsPage from "./pages/AboutUsPage.jsx";
+import PortfolioPage from "./pages/PortfolioPage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
 
 const ParaContainer = styled.div`
   background-image: url(${Variables.background1});
@@ -53,77 +64,16 @@ function App() {
 
   return (
     <>
-      <Navigation backdrop={navBackdrop} setBackdrop={setNavBackdrop} />
-      <SlideOutLink
-        ariaLabel="Jump to services overview"
-        animateStartId="services"
-        scrollY={scrollY}
-        href="#services"
-        text="services"
-      />
-      <Hero src="hero1.mp4" />
-      {/* <ModuleBlender size='large' /> */}
-      <Services />
-      <ModuleBlender />
-      <ParaContainer>
-        <Spacer
-          color={Variables.color20}
-          desktop={258}
-          tablet={120}
-          mobile={100}
-        />
-
-        <ImageContentModule
-          id="designSection"
-          imgSrc="service_about/design.jpg"
-          altTxt="Design"
-          heading="Design"
-          content={`The average Web users attention span lasts on average 8 seconds. With that in my mind, it is vital to provide your websites visitors with the most eye catching content available, while simultaneously delivering your business goals.\n\nBy teaming up with ${Variables.companyName} we can ensure that your users experience a smooth and seamless interface, while still informing users on all that your business has to offer.`}
-        />
-        <Spacer
-          color={Variables.color20}
-          desktop={244}
-          tablet={120}
-          mobile={100}
-        />
-        <ImageContentModule
-          id="developSection"
-          imgPlacement="right"
-          imgSrc="service_about/developer.jpg"
-          altTxt="Developer"
-          heading="Develop"
-          content={`${Variables.companyName} knows Web Development better than anything. With a team full of Web Developers we provide quality and modern wesbites that attract customers to your business.\n\n${Variables.companyName} will develop your website to function to the highest standard. Your user's experience and the user interface will be flawlessly intergrated to show off your business.`}
-        />
-        <Spacer
-          color={Variables.color20}
-          desktop={244}
-          tablet={120}
-          mobile={100}
-        />
-        <ImageContentModule
-          id="supportSection"
-          imgPlacement="left"
-          imgSrc="service_about/support.jpg"
-          altTxt="Support"
-          heading="Support"
-          content={`For new websites built by ${Variables.companyName} we can provide clients with a Content Management System that allows you as the client to make easy content changes to your website, for example: text, images and links.\n\nNot every content update can be made by the client where a developer may be needed. ${Variables.companyName} offers support and maintenance to any website whether built by our team or to an existing website, for example: adding new pages or adjusting the design.`}
-        />
-
-        <Spacer
-          color={Variables.color20}
-          desktop={258}
-          tablet={120}
-          mobile={100}
-        />
-      </ParaContainer>
-      <ModuleBlender />
-      <TestimonialsV2 id="testimonials" />
-      <ModuleBlender />
-      <FaqModule id="faqSection" />
-      <ModuleBlender />
-      <Contact />
-      <ModuleBlender />
-      <Footer />
+      <HashRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </>
   );
 }
