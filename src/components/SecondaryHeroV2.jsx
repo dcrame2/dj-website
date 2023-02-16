2b5f62b56cf59d1636563f59e5542a8b7c899da2
Joshua Tully <jtully97@gmail.com>
2023-02-15 21:10:44 -0600

    update

---
+++ src/components/SecondaryHeroV2.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
+
+const HeroContainer = styled.div``;
+
+const InnerContainer = styled.div`
+    ${Container}
+`;
+
+export default function SecondaryHeroV2() {
+    return <HeroContainer></HeroContainer>;
+}
