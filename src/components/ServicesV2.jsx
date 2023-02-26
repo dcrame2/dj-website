4660f87eb70f30c3c0a8d4d6ad788826ce9a28e1
Joshua Tully <jtully97@gmail.com>
2023-02-26 11:06:14 -0600

    framer animation

--- src/components/ServicesV2.jsx
+++ src/components/ServicesV2.jsx
@@ -4,6 +4,7 @@ import { motion, useScroll, useTransform, useSpring } from 'framer-motion';
 import { Container } from '../styles/Utilities';
 import { Variables } from '../styles/Variables';
 import { H2Styles, PSecondary } from '../styles/Type';
+import { MediaQueries } from '../styles/Utilities';
 
 const Section = styled.section`
     background-color: ${Variables.color3};
@@ -13,20 +14,20 @@ const InnerContainer = styled.div`
     ${Container}
     padding-top: 150px;
     padding-bottom: 150px;
+    display: flex;
+    align-items: center;
+    gap: 75px;
 
-    @media ${Variables.tablet} {
+    @media ${MediaQueries.tablet} {
         padding-top: 120px;
         padding-bottom: 120px;
-    }
-
-    @media ${Variables.tablet} {
-        padding-top: 100px;
-        padding-bottom: 100px;
+        flex-direction: column-reverse;
     }
 `;
 
 const TextContainer = styled.div`
     max-width: 450px;
+
     h2 {
         ${H2Styles}
         overflow: visible;
@@ -39,6 +40,12 @@ const TextContainer = styled.div`
     p {
         ${PSecondary}
     }
+`;
+
+const Img = styled.img`
+    max-width: 680px;
+    width: 100%;
+    height: auto;
 `;
 
 const data = {
@@ -90,13 +97,23 @@ export default function ServicesV2() {
         restDelta: 0.001,
     });
 
+    const imgPercentage = useTransform(scrollYProgress, [0.2, 0.65], [90, 0]);
+
+    const imgPercentageSpring = useSpring(imgPercentage, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    });
+
+    const imgOpacity = useTransform(scrollYProgress, [0.35, 0.4], [0, 1]);
+
     const opacity1 = useTransform(scrollYProgress, [0.2, 0.3], [0, 1]);
 
     const opacity2 = useTransform(scrollYProgress, [0.3, 0.4], [0, 1]);
 
     const opacity3 = useTransform(scrollYProgress, [0.4, 0.5], [0, 1]);
 
-    const paraOpacity = useTransform(scrollYProgress, [0.6, 0.7], [0, 1]);
+    const paraOpacity = useTransform(scrollYProgress, [0.55, 0.7], [0, 1]);
 
     // split heading into spans
     const sentenceArray = data.heading.split(' ');
@@ -160,6 +177,21 @@ export default function ServicesV2() {
                         {data.subheading}
                     </motion.p>
                 </TextContainer>
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        rotateX: 90,
+                        x: 400,
+                    }}
+                    style={{
+                        transformPerspective: '800px',
+                        transformOrigin: 'bottom center',
+                        rotateX: imgPercentageSpring,
+                        opacity: imgOpacity,
+                    }}
+                >
+                    <Img src='/homepage.png' alt='test' />
+                </motion.div>
             </InnerContainer>
         </Section>
     );
